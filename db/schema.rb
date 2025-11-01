9b4adaa20ca9f1bee656342069b492d9e66fd17a
Carlos Rodriguez <ucroxlive@gmail.com>
2025-11-01 12:50:36 -0500

    create a chat room model and unique validation for the name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_01_174232) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_01_174658) do
+  create_table "rooms", force: :cascade do |t|
+    t.string "name"
+    t.boolean "is_private", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.datetime "created_at", null: false
